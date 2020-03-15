0dc49fe68e5a3c8ca6c97314019eb42c8e5fa3a6
Aldair7 <45469841+Aldair7@users.noreply.github.com>
2020-03-14 18:15:16 -0600

    Add files via upload

---
+++ TinSearch/src/test/java/com/kevald/tinsearch/TinSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevald.tinsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
